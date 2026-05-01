21b35495f04ba80da70fbe70d9b0dac9bee1aafe
DavesVenderMais <davi.vendermais@gmail.com>
2026-05-01 15:53:32 -0300

    Atualização 01/05

---
+++ #1 - Transparent Icons.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="#1 - Transparent Icons" tilewidth="32" tileheight="32" tilecount="432" columns="16">
+ <image source="../../../../Downloads/Shikashi's Fantasy Icons Pack v2/#1 - Transparent Icons.png" width="512" height="867"/>
+</tileset>
